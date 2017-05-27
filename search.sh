3220d60900ed74253f978138f1b3f9d8e7e9556c
Michal Hrusecky <Michal@Hrusecky.net>
2017-05-27 14:18:04 +0200

    More fixes and this actually got run

--- search.sh
+++ search.sh
@@ -7,7 +7,8 @@ BASE_URL="http://lists.opensuse.org/"
 NOW="`date +%Y-%m-%d`"
 NOW_S="`date +%s`"
 FIRST_DATE="`date -d "$MONTHS_BACK months ago" +%s`"
-SEND=""
+SEND="y"
+IGNORE_REPLIES=""
 
 ND_WAR="`expr $ND_WAR \* 24 \* 3600`"
 RD_WAR="`expr $RD_WAR \* 24 \* 3600`"
@@ -15,19 +16,20 @@ KICK="`expr   $KICK   \* 24 \* 3600`"
 
 source `pwd`/config
 
+[ "$1" \!= ignore_replies ] || IGNORE_REPLIES=yes
+
 get_lists() {
     wget -O - "$BASE_URL" | \
     sed -n 's|.*style="text-align:left; vertical-align:top;"><a href="\([^"]*\)".*|\1|p'
 }
-
 kick() {
-    ssh $CONNECT_HOST "curl -X POST 'http://connect.opensuse.org/services/api/rest/xml/?method=connect.user.groups.del' -d 'login=opensuse-bot&group_guid=111&api_key=$CONNECT_KEY'"
-    ssh $CONNECT_HOST "curl -X POST 'http://connect.opensuse.org/services/api/rest/xml/?method=connect.user.groups.add' -d 'login=opensuse-bot&group_guid=52448&api_key=$CONNECT_KEY'"
+    ssh $CONNECT_HOST "curl -H 'Host: connect.opensuse.org' -X POST 'http://127.0.0.1/services/api/rest/xml/?method=connect.user.groups.del' -d 'login=$1&group_guid=111&api_key=$CONNECT_KEY'&method=api_auth_key" < /dev/null
+    ssh $CONNECT_HOST "curl -H 'Host: connect.opensuse.org' -X POST 'http://127.0.0.1/services/api/rest/xml/?method=connect.user.groups.add' -d 'login=$1&group_guid=52448&api_key=$CONNECT_KEY'&method=api_auth_key" < /dev/null
 }
 
 restore() {
-    ssh $CONNECT_HOST "curl -X POST 'http://connect.opensuse.org/services/api/rest/xml/?method=connect.user.groups.del' -d 'login=opensuse-bot&group_guid=52448&api_key=$CONNECT_KEY'"
-    ssh $CONNECT_HOST "curl -X POST 'http://connect.opensuse.org/services/api/rest/xml/?method=connect.user.groups.add' -d 'login=opensuse-bot&group_guid=111&api_key=$CONNECT_KEY'"
+    ssh $CONNECT_HOST "curl -H 'Host: connect.opensuse.org' -X POST 'http://127.0.0.1/services/api/rest/xml/?method=connect.user.groups.del' -d 'login=$1&group_guid=52448&api_key=$CONNECT_KEY'&method=api_auth_key" < /dev/null
+    ssh $CONNECT_HOST "curl -H 'Host: connect.opensuse.org' -X POST 'http://127.0.0.1/services/api/rest/xml/?method=connect.user.groups.add' -d 'login=$1&group_guid=111&api_key=$CONNECT_KEY'&method=api_auth_key" < /dev/null
 }
 
 get_mboxes() {
@@ -53,51 +55,60 @@ get_mboxes() {
 
 last_active() {
     LAST_ACTIVE="0"
+    LAST_MAIL_ACTIVE="0"
     for mail in $@; do
-        while read mail_file; do
-            MAIL_DATE="`sed -n 's|^Date:\ ||p' | head -n 1`"
-            MAIL_DATE="`date -d "$MAIL_DATE" +%s`"
-            if [ "$MAIL_DATE" -gt "$LAST_ACTIVE" ]; then
-                LAST_ACTIVE="$MAIL_DATE"
-            fi
-        done < <(grep -Ril -e "^From: .*$mail.*" -e "^Sender: .*$mail.*" mail)
-    done
-    LAST_MAIL_ACTIVE="$LAST_ACTIVE"
-    for i in data/*/*.gz; do
-        MAIL_DATE="`echo "$i" | sed -n 's|.*\([2-9][0-9][0-9][0-9]\)-\([0-1][0-9]\).mbox.gz|\1-\2-01|'`"
-        MAIL_DATE="`date -d "$MAIL_DATE" +%s`"
-        # First day of month + 31 days
-        MAIL_DATE="`expr "$MAIL_DATE" + 2678400`"
-        if [ "$MAIL_DATE" -gt "$LAST_ACTIVE" ] && [ -n "`zgrep -i -m 1 " $mail" $i`" ]; then
-            LAST_ACTIVE="$MAIL_DATE"
+        if expr "x$mail" : x- > /dev/null; then
+            continue
         fi
+	if [ -z "IGNORE_REPLIES" ]; then
+            while read mail_file; do
+                MAIL_DATE="`sed -n 's|^Date:\ ||p' "$mail_file" | head -n 1`"
+                MAIL_DATE="`date -d "$MAIL_DATE" +%s`"
+                if [ "$MAIL_DATE" -gt "$LAST_MAIL_ACTIVE" ]; then
+                    LAST_MAIL_ACTIVE="$MAIL_DATE"
+                fi
+            done < <(grep -Ril -e "^From:.*$mail.*" -e "^Sender:.*$mail.*" mail)
+        fi
+        [ "$LAST_ACTIVE" -gt "$LAST_MAIL_ACTIVE" ] || LAST_ACTIVE="$LAST_MAIL_ACTIVE"
+        for i in data/*/*.gz; do
+    	    MAIL_DATE="`echo "$i" | sed -n 's|.*\([2-9][0-9][0-9][0-9]\)-\([0-1][0-9]\).mbox.gz|\1-\2-01|p'`"
+    	    MAIL_DATE="`date -d "$MAIL_DATE" +%s`"
+    	    # First day of month + 31 days
+    	    MAIL_DATE="`expr "$MAIL_DATE" + 2678400`"
+    	    if [ "$MAIL_DATE" -gt "$LAST_ACTIVE" ] && [ -n "`zgrep -i -m 1 "$mail" $i`" ]; then
+    	        LAST_ACTIVE="$MAIL_DATE"
+    	    fi
+        done
     done
     echo $LAST_ACTIVE:$LAST_MAIL_ACTIVE
 }
 
 get_mail_dump() {
-    if [ -f maildump ]; then
-        ssh $CONNECT_HOST "wget -O - \"http://connect.opensuse.org/services/api/rest/xml/?method=connect.membersadmin.mails&api_key=$CONNECT_KEY\""   | grep -v '^<' >  maildump
-        ssh $CONNECT_HOST "wget -O - \"http://connect.opensuse.org/services/api/rest/xml/?method=connect.exmembersadmin.mails&api_key=$CONNECT_KEY\"" | grep -v '^<' >> maildump
-    fi
+    ssh $CONNECT_HOST "wget -O - \"https://connect.opensuse.org/services/api/rest/xml/?method=connect.membersadmin.mails&api_key=$CONNECT_KEY\""   | grep -v '^<' >  maildump
+    ssh $CONNECT_HOST "wget -O - \"https://connect.opensuse.org/services/api/rest/xml/?method=connect.exmembersadmin.mails&api_key=$CONNECT_KEY\"" | grep -v '^<' >> maildump
 }
 
 START="`date +%s`"
 
+get_mail_dump
 get_mboxes
 
 while read ln; do
     USER="`echo "$ln" | sed 's|^\([^\|]*\)\ \|.*|\1|'`"
     CMAIL="`echo "$ln" | sed 's|^\([^\|]*\)\ \|\ \([^\|]*\)\ .*|\2|'`"
     MAILS="`echo "$ln" | sed 's|.*\|\ \([^\|]*\)$|\1|' | tr ' ' "\n" | sort -u`"
-    LAST_ACTIVE="`last_active $MAILS`"
+    [ x"$USER" \!= xopensuse-bot ] || continue
+    LAST_ACTIVE="`last_active $CMAIL $MAILS`"
     LAST_MAIL_ACTIVE="`echo $LAST_ACTIVE | cut -f 2 -d :`"
     LAST_ACTIVE="`echo $LAST_ACTIVE | cut -f 1 -d :`"
     touch 1st-warnings 2nd-warnings kicked
     if [ $LAST_ACTIVE -gt $FIRST_DATE ]; then
         echo $USER is active
         [ -n "$SEND" ] || continue
-        [ -z "`grep "|$CMAIL\$" 1st-warnings 2nd-warnings kicked`" ] || echo $USER was inactive before
+        if [ -n "`grep "|$CMAIL\$" 1st-warnings 2nd-warnings kicked`" ]; then
+		echo $USER was inactive before
+		echo $CMAIL >> to_unkick.txt
+	fi
         sed -i "/|$CMAIL\$/ d" 1st-warnings
         sed -i "/|$CMAIL\$/ d" 2nd-warnings
         if [ -n "`grep "|$CMAIL\$" kicked`" ] && [ $LAST_MAIL_ACTIVE -gt $FIRST_DATE ]; then
